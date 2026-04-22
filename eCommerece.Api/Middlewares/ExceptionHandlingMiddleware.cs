8da0efdb68109d70a1bac98a2691da6669b4b3c9
LAPTOP-EF879ITF\Ravi Gohil <rgohil@affordablehousing.com>
2026-04-23 00:13:58 +0530

    Add project files.

---
+++ eCommerece.Api/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿namespace eCommerece.Api.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"{ex.GetType().ToString()}: {ex.Message}");
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+                context.Response.ContentType = "application/json";
+                var errorResponse = new { Message = "An unexpected error occurred.", Details = ex.Message };
+                if(ex.InnerException is not null)
+                {
+                    _logger.LogError($"Inner Exception: {ex.InnerException.GetType().ToString()}: {ex.InnerException.Message}");
+                    errorResponse = new { Message = "An unexpected error occurred.", Details = $"{ex.Message} Inner Exception: {ex.InnerException.Message}" };
+                }
+                await context.Response.WriteAsJsonAsync(errorResponse);
+            }
+        }
+    }
+}
---
+++ eCommerece.Api/Program.cs
@@ -0,0 +1,19 @@
+using eCommerece.Api.Middlewares;
+using eCommerece.Infrastructure;
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddInfrastructure();
+builder.Services.AddControllers();
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.UseRouting();
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+
+app.Run();
